f3c7ab65ff01f95409432b1edbd77db00b516391
Ikarows <1254803307@qq.com>
2018-04-12 16:51:52 +0800

    delete file

--- main.js
+++
@@ -1,8 +0,0 @@
-const greeter = require('./greeter.js');
-
-import './main.css';//使用require导入css文件
-import './main2.css';//使用require导入css文件
-
-document.querySelector("#root").appendChild(greeter());
-
-console.log('32432423434324234234234324234234');

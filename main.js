f99b5ec2219f195e9d7930ce2132818ce40ff52f
Ikarows <1254803307@qq.com>
2018-04-12 15:53:33 +0800

    这里是备注信息

---
+++ main.js
@@ -0,0 +1,6 @@
+const greeter = require('./greeter.js');
+
+import './main.css';//使用require导入css文件
+import './main2.css';//使用require导入css文件
+
+document.querySelector("#root").appendChild(greeter());
